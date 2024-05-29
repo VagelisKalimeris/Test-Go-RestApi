a93d3aad39990cb7e8a9dbbd155f767b790e371e
Evangelos Kalimeris <vageliskalimeris@gmail.com>
2024-05-29 11:25:19 +0200

    Update user cases.

--- framework/http_client.py
+++ framework/http_client.py
@@ -1,4 +1,4 @@
-from httpx import get, post
+from httpx import get, post, put, patch, delete
 from assertpy import assert_that
 
 from framework.response_util import readable_json
@@ -33,3 +33,30 @@ class TestClient:
 
         # Extract response body
         return post_resp.json()
+
+    def put(self, path: str, body, status_code: int = 200):
+        # Do call
+        put_resp = put(self.service_base_url + path, headers=self.auth, data=body)
+
+        # Verify expected status code
+        assert_that(put_resp, readable_json(put_resp.json())).has_status_code(status_code)
+
+        # Extract response body
+        return put_resp.json()
+
+    def patch(self, path: str, body, status_code: int = 200):
+        # Do call
+        patch_resp = patch(self.service_base_url + path, headers=self.auth, data=body)
+
+        # Verify expected status code
+        assert_that(patch_resp, readable_json(patch_resp.json())).has_status_code(status_code)
+
+        # Extract response body
+        return patch_resp.json()
+
+    def delete(self, path: str, status_code: int = 204):
+        # Do call
+        delete_resp = delete(self.service_base_url + path, headers=self.auth)
+
+        # Verify expected status code
+        assert_that(delete_resp).has_status_code(status_code)
--- go_rest_tests/route_tests/test_user/test_invalid_user_crud.py
+++ go_rest_tests/route_tests/test_user/test_invalid_user_crud.py
@@ -5,38 +5,6 @@ from framework.response_util import readable_json
 from go_rest_tests.test_data.models import User, UserGender, UserStatus
 
 
-@pytest.mark.parametrize('valid_user', [
-    User('user@random.com', UserGender.male.value, 'John Doe', UserStatus.active.value)
-])
-def test_create_new_valid_user(go_rest_client, valid_user):
-    """
-    Creates user & verifies:
-        - Operation success
-        - Expected response data
-    Retrieves created user & verifies:
-        - User exists
-        - User data are same
-    """
-    new_user_dict = valid_user.__dict__
-
-    # POST new user
-    post_resp = go_rest_client.post('/users', new_user_dict)
-
-    # Verify user creation & POST response data is same as original and retrieve new user id
-    new_user_id = assert_that(post_resp, readable_json(post_resp))\
-        .is_equal_to(new_user_dict, ignore='id')\
-        .extract_key('id')\
-        .val
-
-    # GET new user
-    get_resp = go_rest_client.get(f'/users/{new_user_id}')
-
-    # Verify GET response data is same as original
-    assert_that(get_resp, readable_json(get_resp))\
-        .is_equal_to(new_user_dict, ignore='id')\
-        .has_id(new_user_id)
-
-
 @pytest.mark.parametrize('invalid_user', [
     User('invalid@random.com', 'invalid gender option', 'John Doe', UserStatus.active.value),
     User('invalid@random.com', UserGender.male.value, 'John Doe', 'invalid status option'),
@@ -57,3 +25,6 @@ def test_create_new_invalid_user(go_rest_client, invalid_user):
 
     # POST new user with existing user email and verify 422 code
     go_rest_client.post('/users', new_user_dict, status_code=422)
+
+
+
