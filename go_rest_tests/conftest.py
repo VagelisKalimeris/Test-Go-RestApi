1d94e6d8a7ef2ec6ec45699898b5ad689b8c3bcf
Evangelos Kalimeris <vageliskalimeris@gmail.com>
2024-05-29 15:46:55 +0200

    Abstract user creation/deletion.

--- go_rest_tests/conftest.py
+++ go_rest_tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
+from assertpy import assert_that
 
 from framework.go_rest_client import GoRestTestClient
+from framework.response_util import readable_json
+from go_rest_tests.test_data.models import User, UserGender, UserStatus
+from go_rest_tests.test_data.user_emails import valid_user_email
 
 
 @pytest.fixture(scope='session')
@@ -9,3 +13,47 @@ def go_rest_client():
     Provides test client instance to all module tests.
     """
     yield GoRestTestClient()
+
+
+@pytest.fixture(scope='session')
+def valid_user(go_rest_client):
+    """
+    Valid user for all tests to access and use.
+    Created before and deleted after each run.
+
+    Creates user & verifies:
+        - Expected response data
+    Deletes user & verifies:
+        - Operation success
+    Retrieves deleted user & verifies:
+        - Operation failure
+    """
+    valid_user = User(valid_user_email, UserGender.male.value, 'John Doe', UserStatus.active.value)
+    valid_user_dict = valid_user.__dict__
+
+    # POST new user
+    post_resp = go_rest_client.post('/users', valid_user_dict)
+
+    # Create & save valid user for later tests
+    new_user_id = assert_that(post_resp, readable_json(post_resp)) \
+        .is_equal_to(valid_user.__dict__, ignore='id') \
+        .extract_key('id') \
+        .val
+
+    valid_user_dict['id'] = new_user_id
+
+    yield valid_user_dict
+
+    # DELETE new user
+    go_rest_client.delete(f'/users/{new_user_id}')
+
+    # GET deleted user by id
+    go_rest_client.get(f'/users/{new_user_id}', 404)
+
+
+@pytest.fixture(scope='session')
+def valid_user_id(valid_user):
+    """
+    Abstracts repetitive user id retrieval.
+    """
+    return valid_user['id']
--- go_rest_tests/route_tests/test_user/test_user_crud.py
+++ go_rest_tests/route_tests/test_user/test_user_crud.py
@@ -2,17 +2,14 @@ import pytest
 from assertpy import assert_that
 
 from framework.response_util import readable_json
-from go_rest_tests.test_data.models import User, UserGender, UserStatus
+from go_rest_tests.test_data.models import UserStatus
 
 
-@pytest.mark.parametrize('valid_user', [
-    User('kergddovy@random.com', UserGender.male.value, 'John Doe', UserStatus.active.value)
-])
 class TestUserCRUD:
     """
-    Creates user & verifies:
-        - Expected response data
-    Retrieves created user & verifies:
+    User creation and deletion are tested by global fixture.
+
+    Retrieves valid user & verifies:
         - User exists
         - User data are same
     Retrieves all users & verifies:
@@ -23,69 +20,37 @@ class TestUserCRUD:
     Retrieves updated user & verifies:
         - User exists
         - User data are updated
-    Deletes user & verifies:
-        - Operation success
-    Retrieves deleted user & verifies:
-        - Operation failure
     """
-    @pytest.fixture(scope='function')
-    def new_user_dict(self, valid_user):
-        return valid_user.__dict__
-
-    def test_create_new_user(self, go_rest_client, new_user_dict):
-        # POST new user
-        post_resp = go_rest_client.post('/users', new_user_dict)
-
-        # Verify user creation & POST response data is same as original and retrieve new user id
-        new_user_id = assert_that(post_resp, readable_json(post_resp))\
-            .is_equal_to(new_user_dict, ignore='id')\
-            .extract_key('id')\
-            .val
-
-        # Save new user id for later tests
-        pytest.new_user_id = new_user_id
-
-    def test_get_new_user_by_id(self, go_rest_client, new_user_dict):
+    def test_get_new_user_by_id(self, go_rest_client, valid_user, valid_user_id):
         # GET new user by id
-        get_resp = go_rest_client.get(f'/users/{pytest.new_user_id}')
+        get_resp = go_rest_client.get(f'/users/{valid_user_id}')
 
         # Verify GET user response data is same as original
         assert_that(get_resp, readable_json(get_resp))\
-            .is_equal_to(new_user_dict, ignore='id')\
-            .has_id(pytest.new_user_id)
+            .is_equal_to(valid_user)
 
-    def test_new_user_is_vended_in_unfiltered_users(self, go_rest_client, new_user_dict):
-        # GET new user by id
+    def test_new_user_is_vended_in_unfiltered_users(self, go_rest_client, valid_user):
+        # GET all users
         get_resp = go_rest_client.get('/users/')
-
-        new_user_dict['id'] = pytest.new_user_id
 
         # Verify GET all users response data is same as original
         assert_that(get_resp, readable_json(get_resp))\
-            .contains(new_user_dict)
+            .contains(valid_user)
 
-    def test_new_user_update(self, go_rest_client, new_user_dict):
+    def test_new_user_update(self, go_rest_client, valid_user_id):
         update_info = {'status': UserStatus.inactive.value}
 
         # PUT new user
-        patch_resp = go_rest_client.patch(f'/users/{pytest.new_user_id}', update_info)
+        patch_resp = go_rest_client.patch(f'/users/{valid_user_id}', update_info)
 
         # Verify user update & PUT response data
         assert_that(patch_resp, readable_json(patch_resp)) \
             .has_status(UserStatus.inactive.value)
 
-    def test_get_new_user_after_update(self, go_rest_client, new_user_dict):
+    def test_get_new_user_after_update(self, go_rest_client, valid_user_id):
         # GET new user by id
-        get_resp = go_rest_client.get(f'/users/{pytest.new_user_id}')
+        get_resp = go_rest_client.get(f'/users/{valid_user_id}')
 
         # Verify GET user response data is updated
         assert_that(get_resp, readable_json(get_resp))\
             .has_status(UserStatus.inactive.value)
-
-    def test_delete_new_user(self, go_rest_client, new_user_dict):
-        # DELETE new user
-        go_rest_client.delete(f'/users/{pytest.new_user_id}')
-
-    def test_get_new_user_after_deletion(self, go_rest_client, new_user_dict):
-        # GET deleted user by id
-        go_rest_client.get(f'/users/{pytest.new_user_id}', 404)
